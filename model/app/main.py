6dc6a5f4900e9befc2b23ee2da84d92683470eb1
Diego Santos <161679213+DLGFSS@users.noreply.github.com>
2025-05-12 10:36:46 -0600

    Implementacion de algortimo MOEA NSGA-II

---
+++ model/app/main.py
@@ -0,0 +1,62 @@
+import os
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+from model.core.nsga2 import NSGA2
+
+
+def get_experiment_path(script_path, name="sample"):
+    output_dir = os.path.join(script_path, name)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    return output_dir
+
+if __name__ == "__main__":
+    script_path = os.path.dirname(os.path.abspath(__file__))
+    output_path = get_experiment_path(script_path, name="sample")   
+
+    params = {
+        "name": "nsga2",
+        "label": "NSGA-II",
+        "runs": 2, 
+        "iters": 50,
+        "m_objs": 2,
+        "pop_size": 100,
+        "params_mop": {"name": "dtlz1"},
+        "params_crossover": {"name": "sbx", "prob": 1.0, "eta": 20},
+        "params_mutation": {"name": "polymutation", "prob": 1./6, "eta": 15},
+        "verbose": True,
+    }
+
+    all_objectives = []
+
+    colors = ['green', 'orange']  
+    
+    for run in range(params["runs"]):
+        print(f"Ejecutando la corrida {run+1} de {params['runs']}")
+        moea = NSGA2(params)
+        population, objectives = moea.run()
+
+        if objectives is not None:
+            all_objectives.append(objectives)
+
+            file_path = os.path.join(output_path, f"resultados_run_{run+1}.csv")
+            np.savetxt(file_path, objectives, delimiter=",", header="Objetivo 1, Objetivo 2", comments="")
+            print(f"Resultados guardados en {file_path}")
+        else:
+            print(f"No se generaron resultados en la corrida {run+1}")
+
+    plt.figure(figsize=(8, 6))
+    for i, objectives in enumerate(all_objectives):
+        plt.scatter(objectives[:, 0], objectives[:, 1], label=f'Run {i+1}', alpha=0.7, color=colors[i], s=30)
+
+    plt.xlabel('Objetivo 1')
+    plt.ylabel('Objetivo 2')
+    plt.title('NSGA-II - Frentes de Pareto')
+    plt.legend()
+    plt.grid(True)
+    
+
+    plt.savefig(os.path.join(output_path, "fronts.png"), dpi=300)
+    plt.show()
---
+++ model/problems/problem.py
@@ -0,0 +1,36 @@
+import numpy as np
+by_row = 1
+
+class Problems:
+    @staticmethod
+    def dtlz1_g(Z, k):
+        r = 100 * (k+(((Z - 0.5)**2) - np.cos(20*np.pi*(Z-0.5))).sum(axis=by_row))
+        return r
+    
+    def dtlz1(X, m, M):
+    
+        r = None
+    
+
+        n = X.shape[1]
+        j = M - 1
+        k = n - j
+    
+        Y = X[:, :-k]
+        Z = X[:, -k:]
+    
+        g = Problems.dtlz1_g(Z, k)
+    
+
+        if m == 0:
+            r = (1.0 + g) * 0.5 * (Y.prod(axis=by_row))
+    
+    
+        if m in range(1, M-1):
+            r = (1.0 + g) * 0.5 * (Y[:, :M-m-1].prod(axis=by_row)) * (1 - Y[:, M-m-1])
+    
+    
+        if m == M-1:
+            r = (1.0 + g) * 0.5 * (1 - Y[:, 0])
+
+        return r
