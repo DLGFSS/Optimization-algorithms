689520bc9f5014f101bb452910f97d03f108f630
Diego Santos <161679213+DLGFSS@users.noreply.github.com>
2025-09-01 15:08:49 -0600

    Fix Local Search Benchmark Tests

--- tests/benchmark/test_axo_local_search.py
+++ tests/benchmark/test_axo_local_search.py
@@ -3,19 +3,40 @@ from optikit.algorithms.local_search import LocalSearch
 from axo.contextmanager import AxoContextManager
 from axo.endpoint.manager import DistributedEndpointManager
 import matplotlib.pyplot as plt
+import asyncio
+from axo.storage.services import InMemoryStorageService
 
-    
 @pytest.mark.benchmark(group="axo_local_search")
 def test_axo_Local_search(benchmark):
-    with AxoContextManager.local():
-        ls = LocalSearch(x=50, axo_endpoint_id="axo-endpoint-0")
-        
-        def run_local():
-            ls.persistify()
-            return ls.local().unwrap_or(0)
-
-        result = benchmark.pedantic(run_local, iterations=10, rounds=100)
-
+    async def run_local():
+        with AxoContextManager.local() as rt:
+            ls = LocalSearch(x=50, axo_endpoint_id="axo-endpoint-0")
+            result = await ls.persistify()
+            assert result.is_ok
+            res = ls.local()
+            assert res.is_ok
+            mejor_solucion = res.unwrap()
+        return mejor_solucion
+    def runner():
+        return asyncio.run(run_local())
+    result = benchmark.pedantic(runner, iterations=10, rounds=100)
     assert isinstance(result, (int, float))
+    
 
+@pytest.mark.benchmark(group="axo_local_search_in_memory")
+def test_axo_Local_search_in_memory(benchmark):
+    ss = InMemoryStorageService()
+    async def run_local():
+        with AxoContextManager.local(storage_service=ss) as rt:
+            ls = LocalSearch(x=50, axo_endpoint_id="axo-endpoint-0")
+            result = await ls.persistify()
+            assert result.is_ok
+            res = ls.local()
+            assert res.is_ok
+            mejor_solucion = res.unwrap()
+        return mejor_solucion
+    def runner():
+        return asyncio.run(run_local())
+    result = benchmark.pedantic(runner, iterations=10, rounds=100)
+    assert isinstance(result, (int, float))
 
